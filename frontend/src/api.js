559a79c0e6f5391644f8633ba931f7a0b500fbf0
Palika Herbal <amitprabhakar1651@gmail.com>
2026-08-30 00:04:16 +0530

    Connect frontend to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://palika-herbal.onrender.com/api",
 });
 
 export const signup = (data) => API.post("/auth/signup", data);
